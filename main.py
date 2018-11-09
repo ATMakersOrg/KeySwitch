b2377b1719b6fa941b0e5548384f965903ed2dfe
ATMakers - Bill Binko <32851154+ATMakersBill@users.noreply.github.com>
2018-11-09 02:19:35 -0500

    Adding back the dotstar stuff

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import board
 
 #from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
 
-#import adafruit_dotstar
+import adafruit_dotstar
 from time import sleep, monotonic
 
 import SettingsParser
@@ -15,15 +15,15 @@ settings.read('/settings.ini')
 
 # Used if we do HID output, see below
 # One pixel connected internally!
-#dot = adafruit_dotstar.DotStar(board.APA102_SCK, board.APA102_MOSI, 1, brightness=0.8)
+dot = adafruit_dotstar.DotStar(board.APA102_SCK, board.APA102_MOSI, 1, brightness=0.8)
 
-#def hex2rgb(hexcode):
-#red = int("0x"+hexcode[0:2], 16)
-#    green = int("0x"+hexcode[2:4], 16)
-#    blue = int("0x"+hexcode[4:6], 16)
-#    rgb = (red, green, blue)
-#    print(rgb)
-#    return rgb
+def hex2rgb(hexcode):
+    #added 1 to all for #
+    red = int("0x"+hexcode[1:3], 16)
+    green = int("0x"+hexcode[3:5], 16)
+    blue = int("0x"+hexcode[5:7], 16)
+    rgb = (red, green, blue)
+    return rgb
 
 def handleLongPress(switchCode):
     if (switchCode == 0b000110):
@@ -42,7 +42,8 @@ for p in pins:
 
 
 black = (0, 0, 0)
-onColor = (0xFF, 0, 0xFF)
+modeColor = hex2rgb("#444400")
+white=hex2rgb("#555555") 
 modeDelay = 1
 
 lastCode = 0
@@ -61,7 +62,7 @@ while(True):
         for t in settings.getCurrentMode().triggers:
             if (t.switchCode == switchCode):
                 for a in t.actions:
-#                    dot[0] = onColor
+                    dot[0] = white
                     #print(a.codes)
                     a.perform()
                 break
@@ -75,5 +76,5 @@ while(True):
     lastCode = switchCode
 
 
-    sleep(.1)
-#    dot[0] = black
+    sleep(.15)
+    dot[0] = modeColor
